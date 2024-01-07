134d532e2cdb56a0c37717708baa732daa52a596
Tariq <tariq9853@gmail.com>
2024-01-07 20:05:33 +0100

    New dump and fixed schema

--- discord_messages_table.sql
+++ discord_messages_table.sql
@@ -3,7 +3,9 @@ CREATE TABLE wdl_database.discord_messages (
     MessageId BIGINT,
     ChannelId BIGINT,
     UserId BIGINT,
+    Name VARCHAR(255),  -- Adjust the length as needed
     Content TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
     Timestamp TIMESTAMP,
     PremiumType VARCHAR(50)
 );
+
