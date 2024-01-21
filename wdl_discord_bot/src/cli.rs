9fc791da802d0afa65e4f91e8fd5ea3f45e10ae2
Tariq <tariq9853@gmail.com>
2024-01-21 15:35:18 +0100

    Add: Cli args

--- wdl_discord_bot/src/cli.rs
+++ wdl_discord_bot/src/cli.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use serenity::model::Timestamp;
 
 #[derive(Parser, Clone)]
 #[command(author, version, about, long_about = None)]
@@ -10,4 +11,13 @@ pub struct CliCommands {
     #[clap(short, long)]
     ///The amount of messages required before the bot tries to roll and qoute someone.
     pub roll_amount: Option<usize>,
+
+    #[clap(long, requires("scraping"))]
+    ///Starting scrape from date. Date format like <2028-01-01T00:00:00Z>.
+    pub start_date: Option<Timestamp>,
+
+    #[clap(long, requires("scraping"))]
+    ///Ending scrape at date. Date format like <2028-01-01T00:00:00Z>.
+    pub end_date: Option<Timestamp>,
+
 }
--- wdl_discord_bot/src/main.rs
+++ wdl_discord_bot/src/main.rs
@@ -23,6 +23,8 @@ struct Handler {
     counter: Mutex<usize>,
     roll_amount: Option<usize>,
     scraping: bool,
+    start_date: Option<Timestamp>,
+    end_date: Option<Timestamp>,
 }
 
 impl Handler {
@@ -31,6 +33,8 @@ impl Handler {
         channel_id: ChannelId,
         roll_amount: Option<usize>,
         scraping: bool,
+        start_date: Option<Timestamp>,
+        end_date: Option<Timestamp>,
     ) -> Self {
         Handler {
             db_pool,
@@ -38,6 +42,8 @@ impl Handler {
             counter: Mutex::new(0),
             roll_amount,
             scraping,
+            start_date,
+            end_date
         }
     }
 }
@@ -46,9 +52,23 @@ impl Handler {
 impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, bot: Ready) {
         // let start_date: DateTime<Utc> = Utc::now() - Duration::days(1); // 7 days ago
-        // let end_date: DateTime<Utc> = Utc::now(); // now
-        let start_date = Timestamp::parse("2028-01-01T00:00:00Z").unwrap();
-        let end_date = Timestamp::parse("2028-12-31T23:59:59Z").unwrap();
+        // // let end_date: DateTime<Utc> = Utc::now(); // now
+        // let start_date = Timestamp::parse("2028-01-01T00:00:00Z").unwrap();
+        // let end_date = Timestamp::parse("2028-12-31T23:59:59Z").unwrap();
+
+        let start_date = match self.start_date {
+            Some(start_date) => start_date,
+            None => panic!("Start date not set"),
+        };
+        
+
+        let end_date = match self.end_date {
+            Some(end_date) => end_date,
+            None => panic!("Start date not set"),
+        };
+
+        println!("Using dates: {start_date} and {end_date}");
+
         if self.scraping {
             scraper::scrape_messages(
                 ctx,
@@ -98,7 +118,8 @@ async fn main() {
         Ok((db_pool, discord_token, channel_id)) => {
             // Create an instance of handler and fill its contents
             let handler =
-                Handler::new(db_pool, channel_id, cli_args.roll_amount, cli_args.scraping);
+                Handler::new(db_pool, channel_id, cli_args.roll_amount, cli_args.scraping,
+                cli_args.start_date, cli_args.end_date);
 
             let intents = GatewayIntents::GUILD_MESSAGES
                 | GatewayIntents::DIRECT_MESSAGES
